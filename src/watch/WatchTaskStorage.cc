20eb0d1a25c3802d333e2f736b62ca8b8442a876
Jacek Ewertowski <jacek.ewertowski1@gmail.com>
2020-01-20 23:13:33 +0100

    Remove temporary logs

--- src/watch/WatchTaskStorage.cc
+++ src/watch/WatchTaskStorage.cc
@@ -28,19 +28,13 @@ void WatchTaskStorage::foreach(const std::function<void (WatchTask*)>& executor)
 
 WatchTask* WatchTaskStorage::get_by_domain(std::string& domain)
 {
-  printf("WatchTaskStorage::get_by_domain przed mutexem\n");
   std::lock_guard<std::mutex> guard(mutex);
-  printf("WatchTaskStorage::get_by_domain za mutexem\n");
   for (auto const& task : tasks)
   {
-    printf("WatchTaskStorage::get_by_domain w petli\n");
-    printf("WatchTaskStorage::get_by_domain sprawdzanie %s i %s\n", task->domain().c_str(), domain.c_str());
     if (task->domain() == domain)
     {
-      printf("WatchTaskStorage::get_by_domain znaleziony task\n");
       return task;
     }
   }
-  printf("WatchTaskStorage::get_by_domain za petla\n");
   return nullptr;
 }
