460d2b1785f91cd30479309b5bb4957fe4a1257c
Jacek Ewertowski <jacek.ewertowski1@gmail.com>
2020-01-21 21:42:54 +0100

    Refactor ICMP client (#3)

--- src/network/IcmpClient.h
+++ src/network/IcmpClient.h
@@ -1,23 +1,14 @@
-#ifndef NSFD_ICMP_ICMPCLIENT_H_
-#define NSFD_ICMP_ICMPCLIENT_H_
+#ifndef NSFD_NETWORK_ICMPCLIENT_H_
+#define NSFD_NETWORK_ICMPCLIENT_H_
 
 #include <string>
 #include <stdlib.h>
 #include <netinet/ip_icmp.h>
 
-#define NSFD_ICMP_REQ_SUCCESS 0
-#define NSFD_ICMP_REQ_FAILED -1
-#define NSFD_ICMP_DNS_LOOKUP_FAILED -2
-#define NSFD_ICMP_RAW_SOCK_CREATE_ERROR -3
-
 #define NSFD_ICMP_PKT_SIZE 64
 #define NSFD_ICMP_RECV_TIMEOUT 1
 
-#define NSFD_ICMP_REQ_RETRIES 10
-#define NSFD_ICMP_BACKOFF_TIME_S 1
-
-// ping packet structure
-struct icmp_packet_t
+struct IcmpPacket
 {
   struct icmphdr header;
   char msg[NSFD_ICMP_PKT_SIZE - sizeof(struct icmphdr)];
@@ -32,15 +23,14 @@ struct IcmpResult
 class IcmpClient
 {
 private:
-  unsigned short checksum(void* bytes, int length);
+  // TODO: funkcje send_icmp i execute_request powinny być jedną funkcją
   IcmpResult* send_icmp(int raw_sock_fd, struct sockaddr_in* dest_addr, char* hostname, char* ping_ip, char* rev_host);
   IcmpResult* failed_request();
+  unsigned short checksum(void* bytes, int length);
+  void fill_packet_with_random_data(char* packet_msg, int size);
+
 public:
-  /**
-   * Zwraca true jeśli udaje się nawiązać połączenie z hostem i false w przeciwnym wypadku.
-   * Metoda loguje w konsoli informacje o połączeniu.
-   */
   IcmpResult* execute_request(const std::string& domain);
 };
 
-#endif // NSFD_ICMP_ICMPCLIENT_H_
+#endif // NSFD_NETWORK_ICMPCLIENT_H_
